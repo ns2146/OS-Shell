55c7a32ac0a8a7ab40f15b4f62ef49522c241919
Nick Starkey <ns2146@ship.edu>
2022-03-09 22:18:59 -0500

    Major changes-got to test 11 but just broke back to test 8 implementing dup2()

--- wish.c
+++ wish.c
@@ -3,17 +3,26 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 void commander(char *arg, char **parsed);
 int parser(char *input, char** output);
-int runBuiltin(char **input);
-int runPathSetup(char **parsed);
-char usable_path[150];
-char *path = "/bin";
+int runBuiltin(char **input, int input_file);
+int tryPath(char **parsed);
+void editPath(char **parsed);
+int find_redirection(char **parsed);
+int handle_redirection(char **parsed, int index);
+
+char usable_path[100];
+char *path[50] = { NULL };
+char *output;
 
 int main(int argc, char *argv[]) {
     int currentLine = 1;
     char *parsed[150] = { NULL };
+    path[0] = "/bin"; //initialize first path to bin
 
     if (argc == 1) {
         //INTERACTIVE MODE
@@ -43,9 +52,18 @@ int main(int argc, char *argv[]) {
 
 void commander (char *arg, char **parsed) {
     int length = parser(arg, parsed);
+    int index_p = find_redirection(parsed);
+    int input_file = 0;
 
-    int i = 0;
-
+    if (index_p > 0) {
+        //redirection shenanigans
+        if (handle_redirection(parsed, index_p) == 1) {
+            //we have a redirection and need to use the output file
+            output = parsed[index_p + 1];
+            input_file = open(output, O_RDWR);
+        }
+    }
+    
     if ( strncmp(parsed[0], "cd", 2) == 0 ) {
         if (length != 2) {
             fprintf(stderr, "An error has occurred\n");
@@ -58,31 +76,20 @@ void commander (char *arg, char **parsed) {
         }
         exit(0);
     } else if ( strncmp(parsed[0], "path", 4 ) == 0 ) {
-        if ( length == 1 ) {
-            path = "";
-        } else {
-            path = parsed[1];
-            strcat(path, " ");
-            int i = 2;
-            while (parsed[i] != NULL) {
-                strcat(path, parsed[i]);
-                strcat(path, " ");
-            }
-        }
-        runPathSetup(parsed);
+        editPath(parsed);
     } else if ( strncmp(parsed[0], "pwd", 3) == 0 ) {
         char directory[400];
         getcwd(directory, sizeof(directory));
         printf("%s\n", directory);
     } else if ( strncmp(parsed[0], "ls", 2) == 0 ) {
-        int checker = runPathSetup(parsed);
-        if (checker) {
-            runBuiltin(parsed);
+        if (tryPath(parsed) == 1) {
+            runBuiltin(parsed, input_file);
+        } else {
+            fprintf(stderr, "An error has occurred\n");
         }
     } else {
-        int checker = runPathSetup(parsed);
-        if (checker) {
-            runBuiltin(parsed);
+        if (tryPath(parsed)) {
+            runBuiltin(parsed, input_file);
         } else {
             fprintf(stderr, "An error has occurred\n");
         }
@@ -91,6 +98,7 @@ void commander (char *arg, char **parsed) {
 
 //returns length of output array
 int parser(char *input, char **output) {
+
     char *found;
 
     int i = 0;
@@ -104,7 +112,7 @@ int parser(char *input, char **output) {
     return i;
 }
 
-int runBuiltin(char **input) {
+int runBuiltin(char **input, int input_file) {
     int rc = fork();
 
     if (rc < 0) {
@@ -112,32 +120,102 @@ int runBuiltin(char **input) {
         return 0;
     } else if (rc == 0) {
         //in child
+        if (input_file > 0) {
+            dup2(input_file, STDIN_FILENO);//this is running ls > and breaking stuff I think
+        } 
         execv(usable_path, input);
         exit(-1);
     } else {
         //in parent
         //rc = child's id
+        wait(NULL);
         return 0;
     }
     return 1;
 }
 
-int runPathSetup(char **parsed) {
-    /**
-    For example, when the user types `ls`,
-    and path is set to include both `/bin` and `/usr/bin`, try `access("/bin/ls",
-    X_OK)`. If that fails, try "/usr/bin/ls". If that fails too, it is an error.
-    */
-    char test_path[150];
-    strcpy(test_path, path);
-    strcat(test_path, "/");
-    strcat(test_path, parsed[0]);
+int tryPath(char **parsed) {
+    char test_path[500];
 
-    if ( access(test_path, X_OK) == 0 ) {
-        strcpy(usable_path, test_path);
-        return 1;
-    } else {
-        strcpy(usable_path, "\0");
-        return 0;
+    //copy path and parsed to each item in test path
+    int j = 0;
+    while (path[j] != NULL) {
+        strcpy(test_path, path[j]);
+        strcat(test_path, "/");
+        strcat(test_path, parsed[0]);
+
+        if ( access(test_path, X_OK) == 0 ) {
+            strcpy(usable_path, test_path);
+            return 1;
+        }
+        j++;
     }
+    
+    strcpy(usable_path, "\0");
+    return 0;
+}
+
+void editPath(char **parsed) {
+    //change path to whatever we got in parsed
+    //erase path
+    char *new_path[50] = { NULL };
+
+    int i = 0;
+    while (path[i] != NULL) {
+        path[i] = new_path[i];
+        i++;
+    }
+
+    //check if were given a / or not 
+    if (strncmp(parsed[0], "/", 1) == 0) {
+        //set the path directly
+        int j = 1;
+        while (parsed[j] != NULL) {
+            path[j - 1] = "/";
+            strcat(path[j - 1], parsed[j]); //this might break who knows
+            j++;
+        }
+    } else {
+        //add current directory/whatever
+        char this_dir[256];
+
+        getcwd(this_dir, sizeof(this_dir));
+
+        int j = 1;
+        while (parsed[j] != NULL) {
+            path[j - 1] = this_dir;
+            strcat(path[j - 1], "/");
+            strcat(path[j - 1], parsed[j]); //this might break who knows
+            j++;
+        }
+    }
+}
+
+/**
+ * Return the index of the pipe, otherwise return 0
+ */
+int find_redirection(char **parsed) {
+    int i = 1;
+    while (parsed[i] != NULL) {
+        if (strncmp(parsed[i], ">", 1) == 0) {
+            return i;
+        }
+        i++;
+    }
+    return 0;
+}
+
+int handle_redirection(char **parsed, int index) {
+    //first need to figure out what the output file is so whatever is after parsed[index]
+    if (parsed[index + 1] == NULL) {
+        //no output file given
+        fprintf(stderr, "An error has occurred\n");
+    } else if (parsed[index + 2] != NULL) {
+        //extra output file given
+        fprintf(stderr, "An error has occurred\n");
+    } else {
+        //we have a theoretical output file
+        return 1;
+    }
+    return 0;
 }
